5de2b2c74cdd8cadec05903fb156e48c34ab4f5a
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2015-06-10 22:54:08 +0200

    Show some html

--- src/First.java
+++ src/First.java
@@ -1,17 +1,28 @@
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import alex.CalendarFormatter;
+import alex.CalendarWriter;
+import alex.calendar.Year;
+import alex.formatter.HTMLFormatter;
+import alex.reader.TodayReader;
+import alex.writer.ConsoleWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.AbstractCollection;
+import java.io.OutputStream;
+import java.util.List;
 
 public class First extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp){
         try {
-            resp.getOutputStream().print("<h1>HELLO WORLD</h1>");
+
+            OutputStream out = resp.getOutputStream();
+            CalendarFormatter formatter = new HTMLFormatter();
+            List<Year> today = new TodayReader().getYears();
+            CalendarWriter writer = new ConsoleWriter(out);
+            writer.writeYears(today, formatter);
         } catch (IOException e) {
             e.printStackTrace();
         }
